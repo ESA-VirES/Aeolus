ea29aff079d30f924e3e93df2f5fd0fb0b28d00f
Martin Paces <martin.paces@eox.at>
2026-09-21 21:29:40 +0200

    Local development - upgrade to PG17

--- containers/debian13_p38/database/common.sh
+++ containers/debian13_p38/database/common.sh
@@ -1,13 +1,13 @@
 SOURCE_IMAGE_NAME="debian13"
 SOURCE_IMAGE="$REGISTRY/$SOURCE_IMAGE_NAME:$IMAGE_TAG"
 
-IMAGE_NAME="debian13-postgresql15"
+IMAGE_NAME="debian13-postgresql17"
 IMAGE="$REGISTRY/$IMAGE_NAME:$IMAGE_TAG"
 
 BUILD_OPTIONS="--squash --no-cache --build-arg=SOURCE_IMAGE=$SOURCE_IMAGE"
 CONTAINER_NAME="${POD_NAME:-vires-server}--postgres"
 CREATE_OPTIONS="\
     --pod $POD_NAME \
-    --volume ${POD_NAME:-vires-server}--pgdb-15:/var/lib/postgresql/data \
-    --volume ${POD_NAME:-vires-server}--pgdb-15-logs:/var/log/postgresql \
+    --volume ${POD_NAME:-vires-server}--pgdb-17:/var/lib/postgresql/data \
+    --volume ${POD_NAME:-vires-server}--pgdb-17-logs:/var/log/postgresql \
 "
